c74eff39d584e4cebad76a9f199211c5f8e1b531
Devaswathi <swathidevasenapathy@gmail.com>
2024-06-03 14:42:19 +0530

    added some animation

--- src/app/user/components/home/home.component.ts
+++ src/app/user/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef, Renderer2} from '@angular/core';
 import { HeaderComponent } from "../../../shared/components/header/header.component";
 import { FooterComponent } from "../../../shared/components/footer/footer.component";
 import { CarouselModule } from 'primeng/carousel';
@@ -8,12 +8,26 @@ import { MatIconModule } from '@angular/material/icon';
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, FooterComponent , CarouselModule , MatIconModule]
+    imports: [HeaderComponent, FooterComponent , CarouselModule , MatIconModule ]
 })
 export class HomeComponent {
-    constructor(){
+    constructor(private elRef: ElementRef, private renderer: Renderer2){
 
     }
+
+    @HostListener('window:scroll', ['$event'])
+    onWindowScroll(event: any) {
+    const element = this.elRef.nativeElement.querySelector('.third_home_page_container');
+    const boundingClientRect = element.getBoundingClientRect();
+    if (
+        boundingClientRect.top >= 0 &&
+        boundingClientRect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        this.renderer.addClass(element, 'animatgit__heartBeat');
+    } else {
+        this.renderer.removeClass(element, 'animate__heartBeat');
+    }
+   }
 
     images = [
         { src: "../../../assets/New product with new idea.jpg", content: "Transform client concepts into tangible solutions" },
