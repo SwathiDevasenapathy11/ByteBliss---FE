1a549f61f706526071b7b6585199fcf7e3d79ebf
Devaswathi <swathidevasenapathy@gmail.com>
2024-06-03 17:03:41 +0530

    Completed login form responsive and validation

--- src/app/register/components/login/login.component.ts
+++ src/app/register/components/login/login.component.ts
@@ -3,6 +3,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { PasswordModule } from 'primeng/password';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -10,7 +12,9 @@ import { PasswordModule } from 'primeng/password';
     InputTextModule,
     IconFieldModule,
     InputIconModule,
-    PasswordModule
+    PasswordModule,
+    FormsModule,
+    NgIf
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -21,6 +25,10 @@ export class LoginComponent {
 
   togglePasswordVisibility(): void {
     this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+  }
+
+  authenticateForm(form:any){
+    console.log("Login form", form.value)
   }
 
 }
--- src/app/register/register.module.ts
+++ src/app/register/register.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { RegisterRoutingModule } from './register-routing.module';
-
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RegisterRoutingModule
-  ]
+    RegisterRoutingModule,
+    FormsModule
+  ],
+  exports:[FormsModule]
 })
 export class RegisterModule { }
